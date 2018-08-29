20adaa6e30fe3334187b4d7e537466bb9ab08243
joel.chacon <joel.chacon@cimat.mx>
2018-08-29 14:41:51 -0500

    ..

---
+++ tarea2/codigo/script.R
@@ -0,0 +1,4 @@
+
+data =read.table("wine.data", sep=",")
+names(data) = c("Class","Alcohol", "Malic acid", "Ash","Alcalinity of ash", "Magnesium", "Total phenols", "Flavanoids", "Nonflavanoid phenols", "Proanthocyanins", "Color intensity", "Hue", "OD280/OD315 of diluted wines", "Proline")
+
